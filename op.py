209e8416d998b698e1593d3ffbf0c0c31220fbf6
Mathew Rose <tach47@users.noreply.github.com>
2024-05-30 14:31:03 -0400

    bug fixes with newer version of 1password-cli
    The output returned by `op` has significantly changed, as well as the commands have changed.  This should unbreak the module.

--- op.py
+++ op.py
@@ -5,12 +5,14 @@ import os
 import subprocess
 import sys
 
-import toml
+import tomllib
 
 
 currDir = os.path.dirname(os.path.realpath(__file__))
 
-OP_VARS = toml.load('%s/1password.toml' % currDir)
+
+with open("%s/1password.toml" % currDir, "rb") as f:
+    OP_VARS = tomllib.load(f)
 OP_ACCOUNT = OP_VARS['1password']['account_name']
 OP_DEFAULT_VAULT = OP_VARS['1password']['vault_name']
 OP_VAULT = OP_DEFAULT_VAULT
@@ -19,35 +21,11 @@ VAULT_UUID = ''
 
 
 def initialize():
-    global SESSION_ID
-    try:
-        SESSION_ID = os.environ['OP_SESSION_%s' % OP_ACCOUNT]
-        check_login()
-    except KeyError:
-        check_login()
     get_vault_info(OP_VAULT)
-
-
-def check_login():
-    global SESSION_ID
-    command = 'op signin --raw --session %s' % SESSION_ID
-    status, output = call_op(command, 'plaintext')
-    if status == 0:
-        SESSION_ID = output
-        os.environ['OP_SESSION_%s' % OP_ACCOUNT] = output
-        return True
-    else:
-        try:
-            SESSION_ID = login()
-            if len(SESSION_ID) > 0:
-                return True
-        except:
-            sys.exit('Exit code %s: %s' % (status, output))
 
 
 def get_vault_info(vault_name):
     global VAULT_UUID
-    check_login()
     vaults = get_vaults()
     vault_info = {}
     try:
@@ -74,19 +52,22 @@ def get_vaults():
 
 def get_items(vault_name):
     items = {}
-    command = 'op list items --vault "%s"' % vault_name
+    command = 'op item list --vault "%s" --format=json' % vault_name
     status, output = call_op(command)
     for item in output:
-        items['%s' % item['overview']['title']] = item['uuid']
+        items['%s' % item['title']] = item['id']
     return items
 
 
 def get_item_by_uuid(uuid):
     item = {}
-    command = 'op get item %s' % uuid
+    command = 'op item get %s --format=json' % uuid
     status, output = call_op(command)
-    for field in output['details']['fields']:
-        item['%s' % field['designation']] = field['value']
+    for field in output['fields']:
+        try:
+            item['%s' % field['label']] = field['value']
+        except:
+            next
     return item
 
 
@@ -102,7 +83,7 @@ def search_for_uuid(search):
 
 def login():
     command = 'op signin --raw'
-    status, output = call_op(command, 'plaintext')
+    status, output = call_op(command)
     if status != 0:
         sys.exit('Unable to login')
     else:
@@ -110,7 +91,6 @@ def login():
 
 
 def get_password(search):
-    check_login()
     secret = ''
     uuid = search_for_uuid(search)
     secret = get_item_by_uuid(uuid)
@@ -118,23 +98,19 @@ def get_password(search):
 
 
 def get_username(search):
-    check_login()
     secret = ''
     uuid = search_for_uuid(search)
     secret = get_item_by_uuid(uuid)
     return secret['username']
 
-
-def call_op(command, format='JSON'):
+def call_op(command):
     status, output = subprocess.getstatusoutput(command)
     try:
         if status == 0:
-            if format == 'plaintext':
-                returned_output = output
-            else:
-                returned_output = json.loads(output)
+            returned_output = json.loads(output)
         else:
             returned_output = {}
     except json.decoder.JSONDecodeError:
         print(output)
     return (status, returned_output)
+
